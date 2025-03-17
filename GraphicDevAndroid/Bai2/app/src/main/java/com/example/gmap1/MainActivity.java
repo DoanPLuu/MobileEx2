0aaf69700da7dea2cd94beebefb95613c1f011a0
LuuD04N <doanphongluu082@gmail.com>
2025-03-17 10:37:59 +0700

    App exercise no. 2

--- GraphicDevAndroid/Bai2/app/src/main/java/com/example/gmap1/MainActivity.java
+++ GraphicDevAndroid/Bai2/app/src/main/java/com/example/gmap1/MainActivity.java
@@ -8,17 +8,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private GoogleMap mMap; // Khai báo biến này ở mức class
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng hanoi = new LatLng(21.0285, 105.8542);
+        mMap.addMarker(new MarkerOptions().position(hanoi).title("Hà Nội"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hanoi, 15));
     }
 }
