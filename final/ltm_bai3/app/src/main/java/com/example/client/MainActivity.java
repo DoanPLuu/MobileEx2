079031a514bde3ab1704a7f18b28dfa5075d98bf
Dương Minh Trí <3122410420@sv.sgu.edu.vn>
2025-04-17 22:16:55 +0700

    test

---
+++ final/ltm_bai3/app/src/main/java/com/example/client/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.client;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        view = findViewById(R.id.content);
+        sendMess(view);
+    }
+
+    public void sendMess(View view) {
+        String mess = "hello server";
+        new TCPClient().execute(mess);
+    }
+
+    ;
+
+}
